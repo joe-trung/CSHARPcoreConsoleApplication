a21cf2b7bf3ae3ece905d484545ab6e967137fb8
trand <kieutrungtran@gmail.com>
2023-07-25 16:41:36 -0400

    delegate

--- Program.cs
+++ Program.cs
@@ -270,3 +270,10 @@ using System.Text;
 
 
 
+CalculateDelegate c1 = new CalculateDelegate(DelegateExample.addition);
+CalculateDelegate c2 = new CalculateDelegate(DelegateExample.multiplication);
+
+c1(100);
+Console.WriteLine(DelegateExample.getNumber());
+c2(200);
+Console.WriteLine(DelegateExample.getNumber());
